425b607666af699d510010717b01f60725e5d651
Alexander <134475013+AlexGF0305@users.noreply.github.com>
2023-08-10 23:24:30 -0500

    Se agrego JS
    Se agrego JS

--- contacto.js
+++ contacto.js
@@ -1,8 +1,8 @@
-function lecturaDatos() {
+function Enviar() {
     var nombre = document.getElementById("txtName").value;
     var telefono = document.getElementById("txtTelefono").value;
     var email = document.getElementById("txtEmail").value;
-
+    
     const mensaje = "Nombre: " + nombre + "\nTeléfono: " + telefono + "\nEmail: " + email;
 
     alert(mensaje);
